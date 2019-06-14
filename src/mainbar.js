e992f8baec36110d7c8938cda3d4a74520aecf02
Wesley <wesleykok-@hotmail.com>
2019-06-14 11:35:03 +1000

    Other components

---
+++ src/mainbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./mainbar.css";
+
+export function MenuBar() {
+
+
+
+    return <div>
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
+    </div>
+}
---
+++ src/piano.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import SoundfontProvider from './SoundFontProvider.js';
+import 'react-piano/dist/styles.css';
+import './piano.css';
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+export function LongPiano() {
+    const noteRange = {
+        first: MidiNumbers.fromNote('c3'),
+        last: MidiNumbers.fromNote('b6'),
+    };
+    const firstNote = MidiNumbers.fromNote('c3');
+    const lastNote = MidiNumbers.fromNote('b6');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+        firstNote: firstNote,
+        lastNote: lastNote,
+        keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    });
+    return <div>
+        <SoundfontProvider
+            instrumentName="acoustic_grand_piano"
+            audioContext={audioContext}
+            hostname={soundfontHostname}
+            render={({ isLoading, playNote, stopNote }) => (
+                <Piano className="piano"
+                    noteRange={noteRange}
+                    width={window.innerWidth - 300}
+                    playNote={playNote}
+                    stopNote={stopNote}
+                    disabled={isLoading}
+                    keyboardShortcuts={keyboardShortcuts}
+                />
+            )}
+        />
+    </div>
+
+}
