fcbbd5759f5fc17ed078804594225d45cf0e2f1c
Wesley <wesleykok-@hotmail.com>
2020-06-22 07:45:20 +1000

    Updates

---
+++ precache-manifest.022614db10ecdd5ee79c3bbabadede9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669b6e1a417fafd4d1e5d9d4593bbbd3",
+    "url": "/ReactPorty/index.html"
+  },
+  {
+    "revision": "48e956623a3b9fb3fabc",
+    "url": "/ReactPorty/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "dc78380a9ff4c2950e6b",
+    "url": "/ReactPorty/static/css/main.3b1ed726.chunk.css"
+  },
+  {
+    "revision": "48e956623a3b9fb3fabc",
+    "url": "/ReactPorty/static/js/2.b8a10822.chunk.js"
+  },
+  {
+    "revision": "dc78380a9ff4c2950e6b",
+    "url": "/ReactPorty/static/js/main.ae7eee57.chunk.js"
+  },
+  {
+    "revision": "05bba07980fbb3167048",
+    "url": "/ReactPorty/static/js/runtime~main.5cd9b497.js"
+  },
+  {
+    "revision": "76c38d83fb306439a8299a735b30bbff",
+    "url": "/ReactPorty/static/media/data.76c38d83.jpg"
+  },
+  {
+    "revision": "0516d3fc5e2244d296c04a8e2c52bc36",
+    "url": "/ReactPorty/static/media/yuens.0516d3fc.jpg"
+  }
+]);
