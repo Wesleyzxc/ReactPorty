e9b90f63938b6904692c82b58743b843ef060c21
Wesley <wesleykok-@hotmail.com>
2020-07-23 14:32:11 +1000

    Add resume

--- src/home.js
+++ src/home.js
@@ -2,32 +2,34 @@ import React from "react";
 import "./index.css";
 import { Box, makeStyles, IconButton } from "@material-ui/core";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import DescriptionIcon from "@material-ui/icons/Description";
+import RESUME from "./assets/Resume.pdf";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   box: {
-    textAlign: "center"
+    textAlign: "center",
   },
   image: {
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   CompanyTitle: {
     display: "flex",
     fontStyle: "italic",
     fontWeight: 600,
-    justifyContent: "space-around"
+    justifyContent: "space-around",
   },
   companyInfo: {
     padding: "10px",
     marginRight: "5px",
     marginBottom: "20px",
-    boxShadow: "5px 5px 5px 1px lightgrey"
+    boxShadow: "5px 5px 5px 1px lightgrey",
   },
   icons: {
     "& a:visited": {
-      color: "#1565c0"
-    }
-  }
+      color: "#1565c0",
+    },
+  },
 }));
 
 export function Home() {
@@ -40,6 +42,11 @@ export function Home() {
         student. I also study Data-centric Computing and (Networks and Security)
         as my minor.
       </Box>
+      <IconButton>
+        <a href={RESUME} target="_blank">
+          <DescriptionIcon fontSize="large" />
+        </a>
+      </IconButton>
       <IconButton color="primary" size="small" className={classes.icons}>
         <a href="https://www.linkedin.com/in/wesley-kok-wj/">
           <LinkedInIcon fontSize="large" />
@@ -122,7 +129,7 @@ const companyDeets = [
     workDuration: "March 2018 - Present",
     fileName: "yuens",
     text:
-      "● Saved hundreds of man hours by creating automated processes for migration and cleaning of data to new system.\n● Carried out database management in managing tens of daily customer orders against warehouse inventory and amend faults in inventory list without mistake.\n● Managing multiple aisle of supermarket inventory with customer support and in warehouse quality control."
+      "● Saved hundreds of man hours by creating automated processes for migration and cleaning of data to new system.\n● Carried out database management in managing tens of daily customer orders against warehouse inventory and amend faults in inventory list without mistake.\n● Managing multiple aisle of supermarket inventory with customer support and in warehouse quality control.",
   },
 
   {
@@ -130,13 +137,13 @@ const companyDeets = [
     workDuration: "December 2019 - February 2020",
     fileName: "data",
     text:
-      "● Collaborated with lecturer to work on mining and analysing over 10,000 patents and providing a brief, concise and easy-to-understand result to be interpret and made use of.\n● Use of Python packages such as nltk, sklearn and matplotlib to manipulate data and present data elegantly"
+      "● Collaborated with lecturer to work on mining and analysing over 10,000 patents and providing a brief, concise and easy-to-understand result to be interpret and made use of.\n● Use of Python packages such as nltk, sklearn and matplotlib to manipulate data and present data elegantly",
   },
   {
     companyName: "4impact - Business Analyst Intern",
     workDuration: "June 2019",
     fileName: "report",
     text:
-      "● Worked in a team to conduct in-depth analysis and research of 10 topics to work on and narrow it down to 3 meaningful topics with potential areas to be covered.\n● Presented a 6 page report to advise potential sectors that could present promising clients"
-  }
+      "● Worked in a team to conduct in-depth analysis and research of 10 topics to work on and narrow it down to 3 meaningful topics with potential areas to be covered.\n● Presented a 6 page report to advise potential sectors that could present promising clients",
+  },
 ];
